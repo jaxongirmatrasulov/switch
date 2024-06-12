3df4603ffb997920899e92c6da6c484da2ac4c55
jaxongirmatrasulov <jahongirmatrasulov2000@gmail.com>
2024-06-12 14:59:41 +0500

    switch

--- Program.cs
+++ Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+string userName ; 
+Console.Write("Amalni tanlang:");
+userName = Console.ReadLine();
+
+switch(userName)
+{
+    case "Arifmetik":
+    {
+        int firstNum = 15 ;
+        int secondNum = 20 ;
+        int qoshish = firstNum + secondNum ;
+
+        Console.WriteLine("Natija: " + qoshish);
+        break;
+    }
+
+    case "Geometrik":
+    {
+        decimal pi = 3.14m ;
+        int radius = 2 ;
+        decimal yuza = pi * radius*radius ;
+
+        Console.WriteLine("Natija: " + yuza);
+        break;
+    }
+
+}    
+    
+    
