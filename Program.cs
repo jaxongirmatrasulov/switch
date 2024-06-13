0af4c5d34b48338f74b039b04642e08116ecadd8
jaxongirmatrasulov <jahongirmatrasulov2000@gmail.com>
2024-06-13 20:44:23 +0500

    switch

--- Program.cs
+++ Program.cs
@@ -45,11 +45,69 @@
 
 // pattern matching.
 
-string userName = Console.ReadLine();
-string messages = userName switch 
-{
-    "Elbek" => "You are teacher.",
-    "Jahongir" => "You are student.",
-    _ => "default holati"
-};
-Console.WriteLine(messages);
+// string userName = Console.ReadLine();
+// string messages = userName switch 
+// {
+//     "Elbek" => "You are teacher.",
+//     "Jahongir" => "You are student.",
+//     _ => "default holati"
+// };
+// Console.WriteLine(messages);
+
+//HAFTA KUNLARI.
+
+// string days ;
+// Console.Write("Kiriting: ");
+// days = Console.ReadLine();
+
+
+// switch(days)
+// {
+//     case "1":
+//     {
+//         Console.WriteLine("Dushanba");
+//         break;
+//     }
+//     case "2":
+//     {
+//         Console.WriteLine("Seshanba");
+//         break;
+//     }
+//     case "3":
+//     {
+//         Console.WriteLine("Chorshanba");
+//         break;
+//     }
+//     case "4":
+//     {
+//         Console.WriteLine("Payshanba");
+//         break;
+//     }
+//     case "5":
+//     {
+//         Console.WriteLine("Juma");
+//         break;
+//     }
+//     case "6":
+//     {
+//         Console.WriteLine("Shanba");
+//         break;
+//     }
+//     case "7":
+//     {
+//         Console.WriteLine("Yakshanba");
+//         break;
+//     }
+//     default:
+//         Console.WriteLine("xato");
+//         break;
+    
+// }
+
+for (int i=0; i<5; i++){
+    Console.WriteLine("ha");
+}
+
+string[] cars = {"bmw", "gm", "toyota", "mers", "honda"};
+cars[1] = "ferrari";
+Console.WriteLine(cars[1]);
