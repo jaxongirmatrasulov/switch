bf25274244806d5943183ee446e739329f13e6f5
jaxongirmatrasulov <jahongirmatrasulov2000@gmail.com>
2024-06-15 21:25:42 +0500

    three

--- Program.cs
+++ Program.cs
@@ -4,22 +4,24 @@ string userInput;
 do
 {
     Console.WriteLine("Karra jadval");
-    int number = 1 ;
-    while (number <= 9)
+    int number;
+    string mun = Console.ReadLine();
+    number = Convert.ToInt32(mun);
+    while ( number > 1 && number < 10)
     {
         int multiplayer = 1;
-        while(multiplayer <= 9)
+        while (multiplayer <= 9)
         {
             Console.Write($"{number} x {multiplayer} = {multiplayer * number}; ");
             multiplayer++;
         }
 
-       Console.WriteLine("number: " + number);
+        Console.WriteLine("number: " + number);
         number++;
 
     }
     Console.WriteLine("Do you want continue? (y -> n)");
     userInput = Console.ReadLine();
 }
-while(userInput == "y");
+while (userInput == "y");
 
